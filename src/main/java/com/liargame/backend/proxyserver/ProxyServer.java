6394e864054ee9cb94a0e739c2a10e713ad30f2d
imjaehyeong <dlawogud517@gmail.com>
2024-11-01 15:39:49 +0900

    feat(ProxyServer): TCP 서버 연결 설정
    서버가 시작될 때 TCP 서버와 연결 설정을 하였습니다

--- src/main/java/com/liargame/backend/proxyserver/ProxyServer.java
+++ src/main/java/com/liargame/backend/proxyserver/ProxyServer.java
@@ -2,11 +2,26 @@ package com.liargame.backend.proxyserver;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.io.*;
+import java.net.Socket;
 import java.util.HashSet;
 import java.util.Set;
 @ServerEndpoint("/liargame")
 public class ProxyServer {
     private static Set<Session> clients = new HashSet<>();
+    private static Socket tcpSocket;
+    private static BufferedWriter tcpWriter;
+    private static BufferedReader tcpReader;
+    // 서버가 시작될 때 TCP 서버와 연결 설정
+    static {
+        try {
+            tcpSocket = new Socket("localhost", 12345); // TCP 서버의 주소와 포트
+            tcpWriter = new BufferedWriter(new OutputStreamWriter(tcpSocket.getOutputStream()));
+            tcpReader = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     // 클라이언트가 WebSocket 서버에 연결될 때 호출
     @OnOpen
