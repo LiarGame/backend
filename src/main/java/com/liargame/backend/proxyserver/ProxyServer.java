7192a9a3898b808469197ce60619b794aefbbe4a
imjaehyeong <dlawogud517@gmail.com>
2024-11-01 15:19:57 +0900

    feat(ProxyServer): 기본 웹 소켓 서버 설정 및 클라이언트 연결 수락
    - 엔드포인트 설정
    - build.gradle에 웹소켓, 톰켓서버 의존성 추가

--- src/main/java/com/liargame/backend/proxyserver/ProxyServer.java
+++ src/main/java/com/liargame/backend/proxyserver/ProxyServer.java
@@ -1,4 +1,17 @@
 package com.liargame.backend.proxyserver;
-
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.util.HashSet;
+import java.util.Set;
+@ServerEndpoint("/liargame")
 public class ProxyServer {
+    private static Set<Session> clients = new HashSet<>();
+
+    // 클라이언트가 WebSocket 서버에 연결될 때 호출
+    @OnOpen
+    public void onOpen(Session session) {
+        clients.add(session);  // 클라이언트 세션을 추가
+        System.out.println("새로운 클라이언트가 연결되었습니다: " + session.getId());
+    }
 }
