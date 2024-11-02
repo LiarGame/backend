04572f6d823d77e6e91a144f10d176f94cced2e9
imjaehyeong <dlawogud517@gmail.com>
2024-11-02 17:03:45 +0900

    refactor(ProxyServer): 입출력 스트림 객체 스트림으로 변경

--- src/main/java/com/liargame/backend/proxyserver/ProxyServer.java
+++ src/main/java/com/liargame/backend/proxyserver/ProxyServer.java
@@ -17,8 +17,8 @@ import java.util.logging.Logger;
 public class ProxyServer {
     private static Set<Session> clients = new HashSet<>();
     private static Socket tcpSocket;
-    private static BufferedWriter tcpWriter;
-    private static BufferedReader tcpReader;
+    private static ObjectOutputStream tcpWriter;
+    private static ObjectInputStream tcpReader;
     private static final Logger logger = Logger.getLogger(ProxyServer.class.getName());
 
     // 정적 초기화 블록에서 연결 설정과 스레드 시작 메서드 호출
@@ -31,8 +31,8 @@ public class ProxyServer {
     private static void initializeTcpConnection() {
         try {
             tcpSocket = new Socket("localhost", 10001); // TCP 서버의 주소와 포트
-            tcpWriter = new BufferedWriter(new OutputStreamWriter(tcpSocket.getOutputStream()));
-            tcpReader = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream()));
+            tcpWriter = new ObjectOutputStream(tcpSocket.getOutputStream());
+            tcpReader = new ObjectInputStream(tcpSocket.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -46,20 +46,17 @@ public class ProxyServer {
     @OnOpen
     public void onOpen(Session session) {
         clients.add(session);  // 클라이언트 세션을 추가
-        System.out.println("새로운 클라이언트가 연결되었습니다: " + session.getId());
     }
 
     // WebSocket 클라이언트로부터 메시지를 수신하여 TCP 서버로 전달
     @OnMessage
     public void onMessage(Session session, String message) {
         try {
-            // TCP 서버에 WebSocket 클라이언트의 메시지 전송
-            tcpWriter.write(message + "\n");  // 메시지 끝에 줄바꿈 추가하여 메시지 구분
+            //TODO: 메시지 타입 확인하고 메시지 클래스 인스턴스 생성하기
+            tcpWriter.writeObject(message);
             tcpWriter.flush();
-            System.out.println("TCP 서버로 메시지를 전송했습니다: " + message);
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("TCP 서버로 메시지 전송 중 오류 발생: " + message);
         }
     }
 
@@ -67,7 +64,6 @@ public class ProxyServer {
     @OnClose
     public void onClose(Session session) {
         clients.remove(session);  // 클라이언트 세션을 제거
-        System.out.println("클라이언트 연결이 종료되었습니다: " + session.getId());
 
         // 모든 클라이언트가 종료된 경우 TCP 연결 닫기
         if (clients.isEmpty()) {
@@ -97,7 +93,6 @@ public class ProxyServer {
             if (tcpWriter != null) tcpWriter.close();
             if (tcpReader != null) tcpReader.close();
             if (tcpSocket != null && !tcpSocket.isClosed()) tcpSocket.close();
-            System.out.println("TCP 서버와의 연결이 종료되었습니다.");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -127,13 +122,10 @@ public class ProxyServer {
             case "UNICAST":
                 String targetUsername = json.getString("playerName");
                 // targetUsername과 일치하는 클라이언트 찾기
-                Optional<Session> targetSession = clients.stream()
-                        .filter(session -> session.getUserProperties().get("username").equals(targetUsername))
-                        .findFirst();
+                Optional<Session> targetSession = clients.stream().filter(session -> session.getUserProperties().get("username").equals(targetUsername)).findFirst();
                 targetSession.ifPresent(session -> MessageSender.sendMessage(session, tcpMessage));
                 break;
             default:
-                System.out.println("알 수 없는 action: " + action);
                 break;
         }
     }
