bb382a55ef0f414c9f87c351fb790a08b31c3d8d
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-14 14:16:55 +0900

    feat(GameController): 게임 진행 로직을 구현하기 위한 GameController 클래스 생성 및 게임 시작 로직 구현

--- src/main/java/com/liargame/backend/tcpserver/GameController.java
+++ src/main/java/com/liargame/backend/tcpserver/GameController.java
@@ -1,2 +1,34 @@
-package com.liargame.backend.tcpserver;public class GameController {
+package com.liargame.backend.tcpserver;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GameController {
+    private final GameRoom gameRoom;
+
+
+    public GameController(GameRoom gameRoom) {
+        this.gameRoom = gameRoom;
+    }
+
+    public String startGame() {
+        String word;
+        String liar;
+        TopicEnum topic;
+        List<String> players = gameRoom.getPlayers();
+        if (players.isEmpty()) {
+            return "{ \"action\": \"UNICAST\", \"type\": \"ERROR\", \"message\": \"플레이어가 없습니다.\" }";
+        }
+
+        Collections.shuffle(players);
+        liar = players.get(0);
+
+        topic = TopicEnum.getRandomTopic();
+        word = topic.getRandomWord();
+
+        return String.format(
+                "{ \"action\": \"BROADCAST\", \"type\": \"ROLE_ASSIGN_RESPONSE\", \"liar\": \"%s\", \"topic\": \"%s\", \"word\": \"%s\", \"roomCode\": \"%s\" }",
+                liar, topic.getTopic(), word, gameRoom.getRoomCode()
+        );
+    }
 }
