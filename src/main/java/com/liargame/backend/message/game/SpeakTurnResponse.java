c19cd50acd8c559f88555b9825215ee422c60997
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-16 23:02:07 +0900

    feat(SpeakTurnResponse): 발언을 위한 response message 객체 생성

--- src/main/java/com/liargame/backend/message/game/SpeakTurnResponse.java
+++ src/main/java/com/liargame/backend/message/game/SpeakTurnResponse.java
@@ -1,2 +1,38 @@
-package com.liargame.backend.message.game;public class SpeakTurnResponse {
+package com.liargame.backend.message.game;
+
+import com.liargame.backend.message.Message;
+
+public class SpeakTurnResponse implements Message {
+    private final String action = "BROADCAST";
+    private final String type = "SPEAK_TURN_RESPONSE";
+    private String playerName;
+    private String message;
+    private String roomCode;
+
+    public SpeakTurnResponse(String playerName, String message, String roomCode) {
+        this.playerName = playerName;
+        this.message = message;
+        this.roomCode = roomCode;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getRoomCode() {
+        return roomCode;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
 }
