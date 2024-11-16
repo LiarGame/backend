61dcc51d7b908ffc96fae05d22e265cd5ea3293d
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-16 23:03:01 +0900

    feat(DiscussStartResponse): 토론 시작을 알리기 위한 응답값을 나타내는 message 객체 생성

--- src/main/java/com/liargame/backend/message/game/DiscussStartResponse.java
+++ src/main/java/com/liargame/backend/message/game/DiscussStartResponse.java
@@ -1,2 +1,27 @@
-package com.liargame.backend.message.game;public class DiscussStartResponse {
+package com.liargame.backend.message.game;
+
+import com.liargame.backend.message.Message;
+import com.liargame.backend.tcpserver.TopicEnum;
+
+public class DiscussStartResponse implements Message {
+    private final String action = "BROADCAST";
+    private final String type = "DISCUSS_START_RESPONSE";
+    private String roomCode;
+
+    public DiscussStartResponse(String roomCode) {
+        this.roomCode = roomCode;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    public String getRoomCode() {
+        return roomCode;
+    }
 }
