beb390076c917a04d7022a2cdfd9befdb575f148
imjaehyeong <dlawogud517@gmail.com>
2024-11-18 22:57:29 +0900

    chore(tcpserver) 줄바꿈 삭제

--- src/main/java/com/liargame/backend/tcpserver/TcpServer.java
+++ src/main/java/com/liargame/backend/tcpserver/TcpServer.java
@@ -6,16 +6,15 @@ import java.net.Socket;
 
 public class TcpServer {
     private final GameRoomManager gm = new GameRoomManager();
+
     public void start() {
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(10001);
             System.out.println("접속을 기다립니다.");
-
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Web Socket 연결 성공");
-
                 Thread tcpThread = new Thread(new ClientHandler(socket, gm));
                 tcpThread.start();
             }
