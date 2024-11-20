35dbcf2156165593b6af019e27c929368677e9af
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-20 20:05:53 +0900

    feat(VoteResponse): 투표 응답을 위한 클래스 생성

--- src/main/java/com/liargame/backend/message/game/VoteResponse.java
+++ src/main/java/com/liargame/backend/message/game/VoteResponse.java
@@ -1,2 +1,39 @@
-package com.liargame.backend.message.game;public class VoteResponse {
+package com.liargame.backend.message.game;
+
+import com.liargame.backend.message.Response;
+
+public class VoteResponse implements Response {
+    private final String action = "UNICAST";
+    private final String type = "VOTE_RESPONSE";
+    private String playerName;
+    private String suspect;
+    private String roomCode;
+
+    public VoteResponse(String playerName, String suspect, String roomCode) {
+        this.playerName = playerName;
+        this.suspect = suspect;
+        this.roomCode = roomCode;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public String getSuspect() {
+        return suspect;
+    }
+
+    public String getRoomCode() {
+        return roomCode;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getAction() {
+        return null;
+    }
 }
