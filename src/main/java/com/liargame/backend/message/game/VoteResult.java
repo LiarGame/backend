1b6f7eec775a5fa351dc7747b417fa433c72757a
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-20 20:05:32 +0900

    feat(VoteRequest): 투표 요청을 위한 클래스 생성

---
+++ src/main/java/com/liargame/backend/message/game/VoteResult.java
@@ -0,0 +1,16 @@
+package com.liargame.backend.message.game;
+
+import com.liargame.backend.message.Response;
+
+public class VoteResponse implements Response {
+    private final String action = "BROAD";
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getAction() {
+        return null;
+    }
+}
