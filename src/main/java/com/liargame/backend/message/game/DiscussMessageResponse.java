54b88deca57fb930d95717fa083fe3d100874fb5
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-20 16:29:41 +0900

    feat(DiscussMessageResponse): 토론 메시지 응답 클래스 생성

--- src/main/java/com/liargame/backend/message/game/DiscussMessageResponse.java
+++ src/main/java/com/liargame/backend/message/game/DiscussMessageResponse.java
@@ -1,2 +1,39 @@
-package com.liargame.backend.message.game;public class DiscussMessageResponse {
+package com.liargame.backend.message.game;
+
+import com.liargame.backend.message.Response;
+
+public class DiscussMessageResponse implements Response {
+    private final String action = "BROADCAST";
+    private final String type = "DISCUSS_MESSAGE_RESPONSE";
+    private String playerName;
+    private String message;
+    private String roomCode;
+
+    public DiscussMessageResponse(String playerName, String message, String roomCode) {
+        this.playerName = playerName;
+        this.message = message;
+        this.roomCode = roomCode;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getRoomCode() {
+        return roomCode;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getAction() {
+        return null;
+    }
 }
