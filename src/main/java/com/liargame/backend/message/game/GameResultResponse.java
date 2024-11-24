980588368a21a2c0a18224869f8dbbc62bf0b8db
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-24 20:46:26 +0900

    feat(GameResultResponse): 게임 결과 반환 클래스 생성

--- src/main/java/com/liargame/backend/message/game/GameResultResponse.java
+++ src/main/java/com/liargame/backend/message/game/GameResultResponse.java
@@ -1,2 +1,52 @@
-package com.liargame.backend.message.game;public class GameResultResponse {
+package com.liargame.backend.message.game;
+
+import com.liargame.backend.message.Response;
+import com.liargame.backend.tcpserver.TopicEnum;
+
+public class GameResultResponse implements Response {
+    private final String action = "BROADCAST";
+    private final String type = "GAME_RESULT";
+    private String winner;
+    private String liarName;
+    private TopicEnum topic;
+    private String word;
+    private String roomCode;
+
+    public GameResultResponse(String winner, String liarName, TopicEnum topic, String word, String roomCode) {
+        this.winner = winner;
+        this.liarName = liarName;
+        this.topic = topic;
+        this.word = word;
+        this.roomCode = roomCode;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public String getLiarName() {
+        return liarName;
+    }
+
+    public TopicEnum getTopic() {
+        return topic;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getRoomCode() {
+        return roomCode;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getAction() {
+        return null;
+    }
 }
