5e82f533c92b500def26f40c248f9c47efa9ce4b
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-25 13:06:48 +0900

    fix(RoleAssignResponse): 게임 시작 반환 클래스 필드에 playerList 추가

--- src/main/java/com/liargame/backend/message/game/RoleAssignResponse.java
+++ src/main/java/com/liargame/backend/message/game/RoleAssignResponse.java
@@ -3,15 +3,19 @@ package com.liargame.backend.message.game;
 import com.liargame.backend.message.Response;
 import com.liargame.backend.tcpserver.TopicEnum;
 
+import java.util.List;
+
 public class RoleAssignResponse implements Response {
     private final String action = "BROADCAST";
     private final String type = "ROLE_ASSIGN_RESPONSE";
+    private List<String> playerList;
     private String liar;
     private TopicEnum topic;
     private String word;
     private String roomCode;
 
-    public RoleAssignResponse(String liar, TopicEnum topic, String word, String roomCode) {
+    public RoleAssignResponse(List<String> playerList, String liar, TopicEnum topic, String word, String roomCode) {
+        this.playerList = playerList;
         this.liar = liar;
         this.topic = topic;
         this.word = word;
@@ -20,6 +24,10 @@ public class RoleAssignResponse implements Response {
 
     public String getAction() {
         return action;
+    }
+
+    public List<String> getPlayerList() {
+        return playerList;
     }
 
     public String getLiar() {
