1c3b0127a3f9c4e561af9552cff09db8fca6fe90
JEONWOOJIN <90187250+bingbong916@users.noreply.github.com>
2024-11-25 13:05:57 +0900

    fix(GameResultResponse): 게임 결과 반환에 winner 필드를 리스트로 수정

--- src/main/java/com/liargame/backend/message/game/GameResultResponse.java
+++ src/main/java/com/liargame/backend/message/game/GameResultResponse.java
@@ -3,16 +3,18 @@ package com.liargame.backend.message.game;
 import com.liargame.backend.message.Response;
 import com.liargame.backend.tcpserver.TopicEnum;
 
+import java.util.List;
+
 public class GameResultResponse implements Response {
     private final String action = "BROADCAST";
     private final String type = "GAME_RESULT";
-    private String winner;
+    private List<String> winner;
     private String liarName;
     private TopicEnum topic;
     private String word;
     private String roomCode;
 
-    public GameResultResponse(String winner, String liarName, TopicEnum topic, String word, String roomCode) {
+    public GameResultResponse(List<String> winner, String liarName, TopicEnum topic, String word, String roomCode) {
         this.winner = winner;
         this.liarName = liarName;
         this.topic = topic;
@@ -20,7 +22,7 @@ public class GameResultResponse implements Response {
         this.roomCode = roomCode;
     }
 
-    public String getWinner() {
+    public List<String> getWinner() {
         return winner;
     }
 
