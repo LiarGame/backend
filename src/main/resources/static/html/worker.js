723228ff6c658b9c381083595d99c376f855136f
Hyeon <csh-scl>
2024-12-08 01:03:46 +0900

    fix(worker.js): 웹소켓 연결 블록 잘못된 닫힘 문제 수정

--- src/main/resources/static/html/worker.js
+++ src/main/resources/static/html/worker.js
@@ -58,179 +58,165 @@ onconnect = (e) => {
                 port.postMessage({error: "Failed to load configuration."});
             });
     }
-};
 
+    port.onmessage = (event) => {
+        const {type, playerName} = JSON.parse(event.data);
 
-port.onmessage = (event) => {
-    const {type, playerName} = JSON.parse(event.data);
+        // CREATE_ROOM_REQUEST 처리
+        if (type === "CREATE_ROOM_REQUEST") {
 
-    // CREATE_ROOM_REQUEST 처리
-    if (type === "CREATE_ROOM_REQUEST") {
-
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "CREATE_ROOM_REQUEST",
-                playerName: playerName,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                const request = JSON.stringify({
+                    type: "CREATE_ROOM_REQUEST",
+                    playerName: playerName,
+                });
+                socket.send(request);
+            } else {
+                port.postMessage({ error: "WebSocket not connected" });
+            }
         }
-    }
 
-    // JOIN_REQUEST 처리
-    if (type === "JOIN_REQUEST") {
-        const {roomCode} = JSON.parse(event.data);
+        // JOIN_REQUEST 처리
+        if (type === "JOIN_REQUEST") {
+            const {roomCode} = JSON.parse(event.data);
 
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "JOIN_REQUEST",
-                playerName: playerName,
-                roomCode: roomCode,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                const request = JSON.stringify({
+                    type: "JOIN_REQUEST",
+                    playerName: playerName,
+                    roomCode: roomCode,
+                });
+                socket.send(request);
+            } else {
+                port.postMessage({error: "WebSocket not connected"});
+            }
         }
-    }
 
-    // 재연결 요청
-    if (type === "RECONNECT_REQUEST") {
-        const {roomCode} = JSON.parse(event.data);
-        if (socket) {
-            const request = JSON.stringify({
-                type: "RECONNECT_REQUEST",
-                playerName: playerName,
-                roomCode: roomCode,
-            });
-            socket.send(request);
+        // 재연결 요청
+        if (type === "RECONNECT_REQUEST") {
+            const {roomCode} = JSON.parse(event.data);
+            if (socket) {
+                const request = JSON.stringify({
+                    type: "RECONNECT_REQUEST",
+                    playerName: playerName,
+                    roomCode: roomCode,
+                });
+                socket.send(request);
 
+            }
         }
-    }
 
-    if (type === "START_GAME_REQUEST") {
-        const {playerName, roomCode} = JSON.parse(event.data);
+        if (type === "START_GAME_REQUEST") {
+            const {playerName, roomCode} = JSON.parse(event.data);
 
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "START_GAME_REQUEST",
-                playerName: playerName,
-                roomCode: roomCode,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                const request = JSON.stringify({
+                    type: "START_GAME_REQUEST",
+                    playerName: playerName,
+                    roomCode: roomCode,
+                });
+                socket.send(request);
+            } else {
+                port.postMessage({error: "WebSocket not connected"});
+            }
         }
-    }
 
-    if (type === "SPEAK_REQUEST") {
-        const {playerName, message, roomCode} = JSON.parse(event.data);
+        if (type === "SPEAK_REQUEST") {
+            const {playerName, message, roomCode} = JSON.parse(event.data);
 
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "SPEAK_REQUEST",
-                playerName: playerName,
-                message: message,
-                roomCode: roomCode,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                const request = JSON.stringify({
+                    type: "SPEAK_REQUEST",
+                    playerName: playerName,
+                    message: message,
+                    roomCode: roomCode,
+                });
+                socket.send(request);
+            } else {
+                port.postMessage({error: "WebSocket not connected"});
+            }
         }
-    }
 
-    if (type === "DISCUSS_MESSAGE_REQUEST") {
-        const {playerName, message, roomCode} = JSON.parse(event.data);
+        if (type === "DISCUSS_MESSAGE_REQUEST") {
+            const {playerName, message, roomCode} = JSON.parse(event.data);
 
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "DISCUSS_MESSAGE_REQUEST",
-                playerName: playerName,
-                message: message,
-                roomCode: roomCode,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                const request = JSON.stringify({
+                    type: "DISCUSS_MESSAGE_REQUEST",
+                    playerName: playerName,
+                    message: message,
+                    roomCode: roomCode,
+                });
+                socket.send(request);
+            } else {
+                port.postMessage({error: "WebSocket not connected"});
+            }
         }
-    }
 
-    if (type === "VOTE_START_REQUEST") {
-        const {playerName, roomCode} = JSON.parse(event.data);
+        if (type === "VOTE_START_REQUEST") {
+            const {playerName, roomCode} = JSON.parse(event.data);
 
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "VOTE_START_REQUEST",
-                playerName: playerName,
-                roomCode: roomCode,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                const request = JSON.stringify({
+                    type: "VOTE_START_REQUEST",
+                    playerName: playerName,
+                    roomCode: roomCode,
+                });
+                socket.send(request);
+            } else {
+                port.postMessage({error: "WebSocket not connected"});
+            }
         }
-    }
 
-    if (type === "VOTE_REQUEST") {
-        const {voter, suspect, roomCode} = JSON.parse(event.data);
+        if (type === "VOTE_REQUEST") {
+            const {voter, suspect, roomCode} = JSON.parse(event.data);
 
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "VOTE_REQUEST",
-                voter: voter,
-                suspect: suspect,
-                roomCode: roomCode,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                const request = JSON.stringify({
+                    type: "VOTE_REQUEST",
+                    voter: voter,
+                    suspect: suspect,
+                    roomCode: roomCode,
+                });
+                socket.send(request);
+            } else {
+                port.postMessage({error: "WebSocket not connected"});
+            }
         }
-    }
 
-    if (type === "GUESS_WORD_REQUEST") {
-        const {playerName, message, roomCode, guessWord} = JSON.parse(event.data);
+        if (type === "GUESS_WORD_REQUEST") {
+            const {playerName, message, roomCode, guessWord} = JSON.parse(event.data);
 
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "GUESS_WORD_REQUEST",
-                playerName: playerName,
-                message: message,
-                roomCode: roomCode,
-                guessWord: guessWord,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                const request = JSON.stringify({
+                    type: "GUESS_WORD_REQUEST",
+                    playerName: playerName,
+                    message: message,
+                    roomCode: roomCode,
+                    guessWord: guessWord,
+                });
+                socket.send(request);
+            } else {
+                port.postMessage({error: "WebSocket not connected"});
+            }
         }
-    }
 
-    if (type === "CREATE_ROOM_REQUEST") {
-
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "CREATE_ROOM_REQUEST",
-                playerName: playerName,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
+        if (type === "RESTART_ROOM_REQUEST") {
+            const {playerName, roomCode} = JSON.parse(event.data);
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                const request = JSON.stringify({
+                    type: "RESTART_ROOM_REQUEST",
+                    playerName: playerName,
+                    roomCode: roomCode,
+                });
+                socket.send(request);
+            } else {
+                port.postMessage({error: "WebSocket not connected"});
+            }
         }
-    }
+    };
 
-    if (type === "RESTART_ROOM_REQUEST") {
-        const {playerName, roomCode} = JSON.parse(event.data);
-        if (socket && socket.readyState === WebSocket.OPEN) {
-            const request = JSON.stringify({
-                type: "RESTART_ROOM_REQUEST",
-                playerName: playerName,
-                roomCode: roomCode,
-            });
-            socket.send(request);
-        } else {
-            port.postMessage({error: "WebSocket not connected"});
-        }
+    port.onclose = () => {
+        connections = connections.filter((conn) => conn !== port);
     }
-};
-
-port.onclose = () => {
-    connections = connections.filter((conn) => conn !== port);
 };
